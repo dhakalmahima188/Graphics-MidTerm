beedd98281c0f45e873626d7dfba9056d7df8dbe
mahima <076bct033.mahima@pcampus.edu.np>
2022-07-30 09:27:06 +0545

    prespective workinng

--- github.cpp
+++ github.cpp
@@ -1,31 +1,40 @@
 #include "olcConsoleGameEngine.h"
 using namespace std;
 
-struct vec3d {
+
+struct vec3d
+{
 	float x, y, z;
 };
-struct triangle {
-	vec3d p[3];
 
-}; 
-struct mesh {
-	vector<triangle>tris;
+struct triangle
+{
+	vec3d p[3];
 };
-struct mat4x4 {
+
+struct mesh
+{
+	vector<triangle> tris;
+};
+
+struct mat4x4
+{
 	float m[4][4] = { 0 };
 };
 
- 
-class olcEngine3D : public olcConsoleGameEngine {
-
+class olcEngine3D : public olcConsoleGameEngine
+{
 public:
-	olcEngine3D() {
-		m_sAppName = L"3D Demo"; //unicode ko laagi
-
+	olcEngine3D()
+	{
+		m_sAppName = L"3D Demo";
 	}
+
+
 private:
 	mesh meshCube;
 	mat4x4 matProj;
+
 	float fTheta;
 
 	void MultiplyMatrixVector(vec3d& i, vec3d& o, mat4x4& m)
@@ -40,43 +49,45 @@ private:
 			o.x /= w; o.y /= w; o.z /= w;
 		}
 	}
+
 public:
 	bool OnUserCreate() override
 	{
 		meshCube.tris = {
-			//// SOUTH
-		{ 0.0f, 0.0f, 0.0f,    0.0f, 1.0f, 0.0f,    1.0f, 1.0f, 0.0f }, // yesle triangle initialize garxa
-		{ 0.0f, 0.0f, 0.0f,    1.0f, 1.0f, 0.0f,    1.0f, 0.0f, 0.0f },
 
-		//// EAST                                                      
-		{ 1.0f, 0.0f, 0.0f,    1.0f, 1.0f, 0.0f,    1.0f, 1.0f, 1.0f },
-		{ 1.0f, 0.0f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 0.0f, 1.0f },
+			// SOUTH
+			{ 0.0f, 0.0f, 0.0f,    0.0f, 1.0f, 0.0f,    1.0f, 1.0f, 0.0f },
+			{ 0.0f, 0.0f, 0.0f,    1.0f, 1.0f, 0.0f,    1.0f, 0.0f, 0.0f },
 
-		//// NORTH                                                     
-		{ 1.0f, 0.0f, 1.0f,    1.0f, 1.0f, 1.0f,    0.0f, 1.0f, 1.0f },
-		{ 1.0f, 0.0f, 1.0f,    0.0f, 1.0f, 1.0f,    0.0f, 0.0f, 1.0f },
+			// EAST                                                      
+			{ 1.0f, 0.0f, 0.0f,    1.0f, 1.0f, 0.0f,    1.0f, 1.0f, 1.0f },
+			{ 1.0f, 0.0f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 0.0f, 1.0f },
 
-		//// WEST                                                      
-		{ 0.0f, 0.0f, 1.0f,    0.0f, 1.0f, 1.0f,    0.0f, 1.0f, 0.0f },
-		{ 0.0f, 0.0f, 1.0f,    0.0f, 1.0f, 0.0f,    0.0f, 0.0f, 0.0f },
+			// NORTH                                                     
+			{ 1.0f, 0.0f, 1.0f,    1.0f, 1.0f, 1.0f,    0.0f, 1.0f, 1.0f },
+			{ 1.0f, 0.0f, 1.0f,    0.0f, 1.0f, 1.0f,    0.0f, 0.0f, 1.0f },
 
-		//// TOP                                                       
-		{ 0.0f, 1.0f, 0.0f,    0.0f, 1.0f, 1.0f,    1.0f, 1.0f, 1.0f },
-		{ 0.0f, 1.0f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 1.0f, 0.0f },
+			// WEST                                                      
+			{ 0.0f, 0.0f, 1.0f,    0.0f, 1.0f, 1.0f,    0.0f, 1.0f, 0.0f },
+			{ 0.0f, 0.0f, 1.0f,    0.0f, 1.0f, 0.0f,    0.0f, 0.0f, 0.0f },
 
-		//// BOTTOM                                                    
-		{ 1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f },
-		{ 1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f,    1.0f, 0.0f, 0.0f },
+			// TOP                                                       
+			{ 0.0f, 1.0f, 0.0f,    0.0f, 1.0f, 1.0f,    1.0f, 1.0f, 1.0f },
+			{ 0.0f, 1.0f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 1.0f, 0.0f },
+
+			// BOTTOM                                                    
+			{ 1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f },
+			{ 1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f,    1.0f, 0.0f, 0.0f },
 
 		};
 
+		// Projection Matrix
 		float fNear = 0.1f;
 		float fFar = 1000.0f;
-		float fFov = 90.0f; //field of view 90 degree;
+		float fFov = 90.0f;
 		float fAspectRatio = (float)ScreenHeight() / (float)ScreenWidth();
-		float fFovRad = 1.0f / tanf(fFov * 0.5f) / 180.0f * 3.14149f; //degrees to radins 0.5 le khhai kina multiply gaero
+		float fFovRad = 1.0f / tanf(fFov * 0.5f / 180.0f * 3.14159f);
 
-		 
 		matProj.m[0][0] = fAspectRatio * fFovRad;
 		matProj.m[1][1] = fFovRad;
 		matProj.m[2][2] = fFar / (fFar - fNear);
@@ -84,20 +95,17 @@ public:
 		matProj.m[2][3] = 1.0f;
 		matProj.m[3][3] = 0.0f;
 
-		return  true;
-
-		
-		
+		return true;
 	}
-	bool OnUserUpdate(float fElapsedTime) override {
 
-		Fill(0, 0, ScreenWidth(), ScreenHeight(), PIXEL_SOLID, FG_BLACK); //SCREEN CLEAR GAREKO
-		
+	bool OnUserUpdate(float fElapsedTime) override
+	{
+		// Clear Screen
+		Fill(0, 0, ScreenWidth(), ScreenHeight(), PIXEL_SOLID, FG_BLACK);
 
 		// Set up rotation matrices
 		mat4x4 matRotZ, matRotX;
-		
-		fTheta+= 1.0f * fElapsedTime;
+		fTheta += 1.0f * fElapsedTime;
 
 		// Rotation Z
 		matRotZ.m[0][0] = cosf(fTheta);
@@ -115,11 +123,10 @@ public:
 		matRotX.m[2][2] = cosf(fTheta * 0.5f);
 		matRotX.m[3][3] = 1;
 
-		//triangle draw
-																  //DRAW TRIANGLES
-		for (auto tri : meshCube.tris) {
+		// Draw Triangles
+		for (auto tri : meshCube.tris)
+		{
 			triangle triProjected, triTranslated, triRotatedZ, triRotatedZX;
-			
 
 			// Rotate in Z-Axis
 			MultiplyMatrixVector(tri.p[0], triRotatedZ.p[0], matRotZ);
@@ -137,18 +144,15 @@ public:
 			triTranslated.p[1].z = triRotatedZX.p[1].z + 3.0f;
 			triTranslated.p[2].z = triRotatedZX.p[2].z + 3.0f;
 
-			//2d project
-			MultiplyMatrixVector(tri.p[0], triProjected.p[0], matProj);
-			MultiplyMatrixVector(tri.p[1], triProjected.p[1], matProj);
-			MultiplyMatrixVector(tri.p[2], triProjected.p[2], matProj);
+			// Project triangles from 3D --> 2D
+			MultiplyMatrixVector(triTranslated.p[0], triProjected.p[0], matProj);
+			MultiplyMatrixVector(triTranslated.p[1], triProjected.p[1], matProj);
+			MultiplyMatrixVector(triTranslated.p[2], triProjected.p[2], matProj);
 
-			//scale to viewing area
-			///shifting betn 0 and 2
-
+			// Scale into view
 			triProjected.p[0].x += 1.0f; triProjected.p[0].y += 1.0f;
 			triProjected.p[1].x += 1.0f; triProjected.p[1].y += 1.0f;
 			triProjected.p[2].x += 1.0f; triProjected.p[2].y += 1.0f;
-
 			triProjected.p[0].x *= 0.5f * (float)ScreenWidth();
 			triProjected.p[0].y *= 0.5f * (float)ScreenHeight();
 			triProjected.p[1].x *= 0.5f * (float)ScreenWidth();
@@ -156,20 +160,27 @@ public:
 			triProjected.p[2].x *= 0.5f * (float)ScreenWidth();
 			triProjected.p[2].y *= 0.5f * (float)ScreenHeight();
 
-			DrawTriangle(
-				triProjected.p[0].x, triProjected.p[0].y,
+			// Rasterize triangle
+			DrawTriangle(triProjected.p[0].x, triProjected.p[0].y,
 				triProjected.p[1].x, triProjected.p[1].y,
 				triProjected.p[2].x, triProjected.p[2].y,
 				PIXEL_SOLID, FG_WHITE);
 
 		}
-		return  true;
+
+
+		return true;
 	}
+
 };
-int main(){
+
+
+
+
+int main()
+{
 	olcEngine3D demo;
-	if (demo.ConstructConsole(256, 240, 3, 3)) {
+	if (demo.ConstructConsole(256, 240, 3, 3))
 		demo.Start();
-	}
 	return 0;
-	}
+}
